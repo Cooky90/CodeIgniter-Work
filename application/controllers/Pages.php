4b042630864239180fa927c0a5209b95242d0f23
Ashley Cook <ashley.m.d.cook@gmail.com>
2018-10-30 00:30:19 +0000

    Master view concept added

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -12,7 +12,13 @@ class Pages extends CI_Controller
 	{
 		$data['title'] = 'Home Page';
 		$data['message'] = 'Hello World From The Controller';
+
+/*		$this->load->view('Pages/Elements/header', $data);
+		$this->load->view('Pages/Elements/navigation', $data);
 		$this->load->view('Pages/home', $data);
+		$this->load->view('Pages/Elements/footer', $data);*/
+
+		$this->load->view('Layouts/master', $data);
 	}
 }
 ?>
---
+++ application/views/Pages/Elements/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css"
+	href="<?php echo base_url();?>assets/css/bootstrap.min.css" />
+
+	<link rel="stylesheet" type="text/css"
+	href="<?php echo base_url();?>assets/css/spacing.css" />
+
+	<link rel="stylesheet" type="text/css"
+	href="<?php echo base_url();?>assets/css/custom.css" />
+
+	<title><?php echo $title; ?></title>
+</head>
+<body>
