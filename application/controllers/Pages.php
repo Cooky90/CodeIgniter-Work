c3920748d324eddfb115aba7fd8cad542c97a728
Ashley Cook <ashley.m.d.cook@gmail.com>
2018-10-31 00:28:21 +0000

    dbforge work

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -12,12 +12,29 @@ class Pages extends CI_Controller
 	{
 		$data['title'] = 'Home Page';
 		$data['message'] = 'Hello World From The Controller';
+		$data['content'] = 'Pages/home';
 
 /*		$this->load->view('Pages/Elements/header', $data);
 		$this->load->view('Pages/Elements/navigation', $data);
 		$this->load->view('Pages/home', $data);
 		$this->load->view('Pages/Elements/footer', $data);*/
 
+		$this->load->view('Layouts/master', $data);
+	}
+
+	public function about()
+	{
+		$data['title'] = 'About Page';
+		$data['message'] = 'Hello World From The Controller About';
+		$data['content'] = 'Pages/about';
+		$this->load->view('Layouts/master', $data);
+	}
+
+	public function profile()
+	{
+		$data['title'] = 'Profile';
+		$data['message'] = 'Hello World From The Controller Profile';
+		$data['content'] = 'Pages/profile';
 		$this->load->view('Layouts/master', $data);
 	}
 }
--- application/views/Pages/home.php
+++ application/views/Pages/home.php
@@ -1,4 +1,3 @@
-	<div class="container">
 		<div class="jumbotron m-top-60">
 		  <h1 class="display-4"><?php echo $message; ?></h1>
 		  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
@@ -6,4 +5,3 @@
 		  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
 		  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 		</div>
-	</div>
